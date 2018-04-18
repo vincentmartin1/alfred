c7ff8bec2a4228e6db8ba841bbfd3751d7f33940
Anacomb <huugoo60de@gmail.com>
2018-04-18 16:18:05 -0400

    ajout de la dépendance + classe TextToSpeech

---
+++ app/src/main/java/equipe16/infomobile/uqac/alfred/TextToSpeech.java
@@ -0,0 +1,15 @@
+package equipe16.infomobile.uqac.alfred;
+
+import java.util.Locale;
+
+import marytts.LocalMaryInterface;
+import marytts.MaryInterface;
+import marytts.util.data.audio.MaryAudioUtils;
+
+public class TextToSpeech {
+
+
+    public void talk(String sample){
+
+    }
+}
