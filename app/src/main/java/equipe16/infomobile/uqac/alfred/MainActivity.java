278c14d09026d4ea060481aafe7909b733bf7cf1
vincent_martin1 <vincent_martin1@ens.univ-artois.fr>
2018-04-18 13:07:31 -0400

    Avancements brain.rive

--- app/src/main/java/equipe16/infomobile/uqac/alfred/MainActivity.java
+++ app/src/main/java/equipe16/infomobile/uqac/alfred/MainActivity.java
@@ -13,10 +13,15 @@ import org.apache.commons.io.IOUtils;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     RiveScript bot;
+    EditText editText;
+    TextView answer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,11 +29,11 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
         // RiveScript bot initialization
         bot = new RiveScript();
+        // bot.setHandler("javascript", new JavaScriptHandler());
 
-        InputStream inputStream = getResources().openRawResource(R.raw.helloworld);
+        InputStream inputStream = getResources().openRawResource(R.raw.brain);
         String script = "";
 
         try {
@@ -37,21 +42,60 @@ public class MainActivity extends AppCompatActivity {
             Log.i("ALF", e.getMessage());
         }
 
-        Log.i("ALF", "Loaded script : \n" + script);
+        //Log.i("ALF", "Loaded script : \n" + script);
         bot.stream(script);
 
         bot.sortReplies();
+
+        editText = findViewById(R.id.question);
+        answer = findViewById(R.id.answer);
     }
 
     public void submit(View view) {
-        EditText editText = findViewById(R.id.question);
-        TextView answer = findViewById(R.id.answer);
-
         String question = editText.getText().toString();
+        String prefix;
+        String content;
         if (question.length() == 0)
             answer.setText("Question vide !");
-        else
+        else {
+            String reply = bot.reply("user", question);
+            String[] arr = reply.split(";");
+
+            prefix = arr[0];
+
+            // On retire le premier élément de la liste (le préfixe)
+            List<String> list = new ArrayList<>(Arrays.asList(reply));
+            list.remove(0);
+            arr = list.toArray(new String[0]);
+
+            // Concaténation de la liste pour reformer le contenu
+            StringBuilder sb = new StringBuilder();
+            for(String s:
+                    arr) {
+                sb.append(s);
+            }
+            content = sb.substring(0, sb.length() - 1);
+
+            Log.i("ALF", "Prefix : " + prefix);
+            Log.i("ALF", "Content : " + prefix);
+
+            // Test du type de la réponse et exécution
+            if (prefix.equals("REP"))
+                executeReply(content);
+            else if (prefix.equals("CMD"))
+                executeCommand(content);
+
             answer.setText(bot.reply("user", question));
+        }
+    }
+
+    public void executeReply(String reply) {
+        Log.i("ALF", "Reply");
+        answer.setText(reply);
+    }
+
+    public void executeCommand(String cmd) {
+
     }
 
 }
